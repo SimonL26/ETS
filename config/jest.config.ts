241640fd9b914b4a4549e898be17f19f1a1585df
Simone Lin <806985104@qq.com>
2026-03-11 09:09:23 +0100

    Re-organized repository structure

--- config/jest.config.ts
+++ config/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  rootDir: "..",
   roots: ["<rootDir>/src"],
   testMatch: ["**/__tests__/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
